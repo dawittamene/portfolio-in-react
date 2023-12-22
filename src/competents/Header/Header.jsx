2dcef55e34ea24dc9dcec81ad9138a63b924f44c
dawittamene <dawittamene559@gmail.com>
2023-12-22 21:14:53 +0300

    add cv pdf to my portfolio

--- src/competents/Header/Header.jsx
+++ src/competents/Header/Header.jsx
@@ -6,7 +6,7 @@ const Header = () => {
       <div className="container header__container">
         <h5>Hello I'm</h5>
         <h1>Dawit Tamene</h1>
-        <h5 className="text-light">ggg</h5>
+        <h5 className="text-light">FullStack Developer</h5>
 
       </div>
       
