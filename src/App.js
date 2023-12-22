09a82986739de93598f62209b99efe3dec5ab148
dawittamene <dawittamene559@gmail.com>
2023-12-22 19:35:36 +0300

    commit

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import Nav from './competents/Nav/Nav'
+import Header from './competents/Header/Header'
+import About from './competents/About/About'
+import Expriance from './competents/Expriance/Expriance'
+import Service from './competents/Service/Service'
+
+
+
+function App() {
+  return (
+    <div className="App">
+      
+      <Header/>
+      <Nav/>
+      <About/>
+      <Expriance/>
+      <Service/>
+     
+    </div>
+  );
+}
+
+export default App;
---
+++ src/competents/Expriance/Expriance.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Expriance = () => {
+  return (
+    <div>
+      Expriance
+    </div>
+  )
+}
+
+export default Expriance
