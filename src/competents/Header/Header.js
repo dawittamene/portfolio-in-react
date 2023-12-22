4a040f624fb4e8f7a48b0fe4a9516a72264bb4d0
dawittamene <dawittamene559@gmail.com>
2023-12-22 21:03:47 +0300

    add general css class

--- src/competents/Header/Header.js
+++ src/competents/Header/Header.js
@@ -2,9 +2,17 @@ import React from 'react'
 
 const Header = () => {
   return (
-    <div>
-      Header
-    </div>
+    <header>
+      <div className="container header__container">
+        <h5>Hello I'm</h5>
+        <h1>Dawit Tamene</h1>
+        <h5 className="text-light">ggg</h5>
+
+      </div>
+      
+
+    </header>
+   
   )
 }
 
