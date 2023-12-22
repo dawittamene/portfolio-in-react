5038cab2e4363816a712f668bf70f6ca787c4d3b
dawittamene <dawittamene559@gmail.com>
2023-12-22 15:22:20 +0300

    change some thing

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,9 @@
+import React from 'react'
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-     App
-    </div>
-  );
+    <div>App</div>
+  )
 }
 
-export default App;
+export default App
