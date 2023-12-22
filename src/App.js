b4c8a61019c9a831e6ff0457844ecee249855396
dawittamene <dawittamene559@gmail.com>
2023-12-22 19:55:23 +0300

    add some components

--- src/App.js
+++ src/App.js
@@ -3,6 +3,12 @@ import Header from './competents/Header/Header'
 import About from './competents/About/About'
 import Expriance from './competents/Expriance/Expriance'
 import Service from './competents/Service/Service'
+import Portfolio from './competents/Portfolio/Portfolio'
+import Testimonial from './competents/Testimonial/Testimonial'
+import Contact from './competents/Contact/Contacts'
+import Footer from './competents/Footer/Footer'
+
+
 
 
 
@@ -15,6 +21,10 @@ function App() {
       <About/>
       <Expriance/>
       <Service/>
+      <Portfolio/>
+      <Testimonial/>
+      <Contact/>
+      <Footer/>
      
     </div>
   );
