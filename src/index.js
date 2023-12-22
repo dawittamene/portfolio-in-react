88f1aea4456476e88a0a0fc95ec0b8e457ec5bac
dawittamene <dawittamene559@gmail.com>
2023-12-22 14:15:38 +0300

    add assets folder

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import App from './App.jsx';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
@@ -9,6 +9,4 @@ root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
-);
-
-
+)
